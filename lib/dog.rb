fb92e98e1bc9b9d1b8a49237086d495a8b8c2d32
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-04-21 19:10:44 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require "pry"
+class Dog
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+    def 
+    def name=(name)
+        @name = name
+    end 
+    def name 
+        @name
+    end 
+    def breed=(breed)
+        @breed = breed
+    end
+     
+    def breed
+        @breed 
+    end
+end
+
